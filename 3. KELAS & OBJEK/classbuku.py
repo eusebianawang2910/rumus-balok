ee518a6111e5cf0691f3bcd5b76f1c8f08c77457
eusebianawang2910 <97903959+eusebianawang2910@users.noreply.github.com>
2022-02-26 02:47:36 +0700

    Delete 3. KELAS & OBJEK directory

--- 3. KELAS & OBJEK/classbuku.py
+++
@@ -1,9 +0,0 @@
-class Buku:
-    def __init__(self, judul, pengarang, tahun_terbit):
-        self.judul = judul
-        self.pengarang = pengarang
-        self.tahun_terbit = tahun_terbit
-
-
-buku = Buku('Tenggelamnya Kapal Van Der Wijck', 'Hamka', 1938)
-print(f'Buku {buku.judul} karangan {buku.pengarang} pertama kali diterbitkan tahun {buku.tahun_terbit}')
