0987e84d030fb85a246c5cc3285a7d1aba5f24da
eusebianawang2910 <97903959+eusebianawang2910@users.noreply.github.com>
2022-02-26 23:20:32 +0700

    TUGAS 3
    Tugas 3 Kelas & Objek

---
+++ 3. Kelas & Objek/latihan/classgaris.py
@@ -0,0 +1,23 @@
+class Titik:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Garis:
+    def __init__(self, titikPertama, titikKedua):
+        self.titikPertama = titikPertama
+        self.titikKedua = titikKedua
+
+    def panjang(self):
+        pjgX = self.titikKedua.x - self.titikPertama.x
+        pjgY = self.titikKedua.y - self.titikPertama.y
+        pjg = (pjgX**2 + pjgY**2) ** 0.5
+        return pjg
+
+
+titik_a = Titik(0, 0)
+titik_b = Titik(3, 4)
+
+garis_ab = Garis(titik_a, titik_b)
+print('Panjang garis ab adalah ', garis_ab.panjang())
